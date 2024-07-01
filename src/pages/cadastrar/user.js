b23c8d15aeda5afd01d8c4b677362492580a60b1
DiogoWebber <diogoevaldt17@gmail.com>
2024-07-01 18:31:43 -0300

    front

--- src/pages/cadastrar/user.js
+++ src/pages/cadastrar/user.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import Link from 'next/link'; 
-import styles from '../../styles/styles.module.css'; 
+import Link from "next/link";
+import styles from "../../styles/styles.module.css";
 
 export default function Home() {
   const [users, setUsers] = useState([]);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [errorAlert, setErrorAlert] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
+  const [successAlert, setSuccessAlert] = useState("");
 
   useEffect(() => {
     fetchUsers();
@@ -16,11 +17,11 @@ export default function Home() {
 
   const fetchUsers = async () => {
     try {
-      const response = await axios.get('http://localhost:3001/user/users');
+      const response = await axios.get("http://localhost:3001/user/users");
       console.log(response.data);
       setUsers(response.data);
     } catch (error) {
-      console.error('Error fetching users:', error);
+      console.error("Error fetching users:", error);
     }
   };
 
@@ -30,69 +31,92 @@ export default function Home() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
+
     try {
-      const response = await axios.post('http://localhost:3001/user/create-user', {
-        username,
-        password,
-        email
-      }, {
-        headers: {
-          'Content-Type': 'application/json'
+      const response = await axios.post(
+        "http://localhost:3001/user/create-user",
+        {
+          username,
+          password,
+          email,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-      });
+      );
       console.log(response.data);
       // Limpa os campos do formulário após a criação bem-sucedida
-      setUsername('');
-      setPassword('');
-      setEmail('');
+      setUsername("");
+      setPassword("");
+      setEmail("");
+      // Exibe a mensagem de sucesso
+      setSuccessAlert("Usuário criado com sucesso!");
+      setTimeout(() => {
+        setSuccessAlert("");
+      }, 3000); // Limpa o alerta após 3 segundos
       // Atualiza a lista de usuários após a criação bem-sucedida
       fetchUsers();
     } catch (error) {
-      console.error('Error creating user:', error);
+      console.error("Error creating user:", error);
       if (error.response && error.response.status === 400) {
-        setErrorAlert('Usuário já existe!'); // Exemplo de mensagem de erro específica
+        setErrorAlert("Usuário já existe!"); // Exemplo de mensagem de erro específica
         // Você pode adicionar lógica adicional aqui para lidar com diferentes tipos de erros
       } else {
-        setErrorAlert('Erro ao criar usuário.');
+        setErrorAlert("Erro ao criar usuário.");
       }
       setTimeout(() => {
-        setErrorAlert('');
+        setErrorAlert("");
       }, 3000); // Limpa o alerta após 3 segundos
     }
   };
 
   return (
     <div className={styles.containerUsers}>
-      {errorAlert && <div className="alert alert-danger">{errorAlert}</div>}
-      <div> 
-      <div className={styles.containerMeio}>
-  <h2>CRIAR USUÁRIO</h2>
-  <form onSubmit={handleSubmit}>
-    <label>
-     Usuário:
-      <input type="text" value={username} onChange={handleUsernameChange} required />
-    </label>
-    <label>
-      Senha:
-      <input type="password" value={password} onChange={handlePasswordChange} required />
-    </label>
-    <label>
-      Email:
-      <input type="email" value={email} onChange={handleEmailChange} required />
-    </label>
-    <button type="submit">Criar usuário</button>
-  </form>
+      {errorAlert && <div className={`${styles.alert} ${styles["alert-danger"]}`}>{errorAlert}</div>}
+      {successAlert && <div className={`${styles.alert} ${styles["alert-success"]}`}>{successAlert}</div>}
+      <div>
+        <div className={styles.containerMeio}>
+          <h2>CRIAR USUÁRIO</h2>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Usuário:
+              <input
+                type="text"
+                value={username}
+                onChange={handleUsernameChange}
+                required
+              />
+            </label>
+            <label>
+              Senha:
+              <input
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+              />
+            </label>
+            <label>
+              Email:
+              <input
+                type="email"
+                value={email}
+                onChange={handleEmailChange}
+                required
+              />
+            </label>
+            <button type="submit">Criar usuário</button>
+          </form>
 
-  <div className="link-container">
-    <Link href="/">
-      <button>Voltar para Admin</button>
-    </Link>
-  </div>
-</div>
-
+          <div className="link-container">
+            <Link href="/">
+              <button>Voltar para Admin</button>
+            </Link>
+          </div>
         </div>
+      </div>
     </div>
-
   );
 }
